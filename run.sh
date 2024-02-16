ead4ca211dc629cfaeb321af652927b3c48a10e8
jakubovciak <jakubovciak@gmail.com>
2024-02-16 11:55:42 +0100

    added run.sh, minor changes

---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -e
+
+echo "
+Started annotation workflow...
+"
+
+Rscript R_demo.R
+
+echo "
+Finished annotation workflow, check figures in Results directory
+"
+
+cd Transitions
+
+echo "
+Started celltype transitions workflow...
+"
+
+Rscript -e "rmarkdown::render(input = '01_Integration.Rmd', output_dir = '../Results', clean = TRUE)"
+
+Rscript 02_Convert_to_anndata.R
+
+source /root/miniconda3/etc/profile.d/conda.sh
+
+conda activate ctk_pseudotime_env
+
+python 03_Cytotrace_pseudotime.py
+
+Rscript -e "rmarkdown::render(input = '04_Urd_transition_matrix.Rmd', output_dir = '../Results', clean = TRUE)"
+
+Rscript -e "rmarkdown::render(input = '05_Transition_graphs.Rmd', output_dir = '../Results', clean = TRUE)"
+
+echo "
+Finished celltype transitions workflow, check rendered reports in Results directory
+"
+
+cd ..
